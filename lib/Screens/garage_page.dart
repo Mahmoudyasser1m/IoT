8fcb5d214d8d3203e83d7435018ff11376386fa7
Mahmoud <mahmoudy1567@gmail.com>
2024-03-01 21:29:43 +0200

    2

---
+++ lib/Screens/garage_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:iot/constants.dart';
+
+
+
+class GaragePage extends StatelessWidget {
+  const GaragePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: kPrimaryColor,
+      ),
+    );
+  }
+}
